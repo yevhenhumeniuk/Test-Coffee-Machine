f694896cab5ae0c4d45315811469d5674f644e0d
Yevhen Humeniuk <gumeniukyevgen@gmail.com>
2022-02-06 15:41:52 +0200

    updated the integration tests

--- tests/Integration/Console/MakeDrinkCommandTest.php
+++ tests/Integration/Console/MakeDrinkCommandTest.php
@@ -1,13 +1,32 @@
 <?php
 
+declare(strict_types=1);
+
 namespace GetWith\CoffeeMachine\Tests\Integration\Console;
 
 use GetWith\CoffeeMachine\Console\MakeDrinkCommand;
 use GetWith\CoffeeMachine\Tests\Integration\IntegrationTestCase;
 use Symfony\Component\Console\Tester\CommandTester;
 
+/**
+ * @internal
+ * @coversNothing
+ */
 class MakeDrinkCommandTest extends IntegrationTestCase
 {
+    public const COMMAND = 'app:order-drink';
+
+    public const TEA_DRINK_NAME = 'tea';
+    public const COFFEE_DRINK_NAME = 'coffee';
+    public const CHOCOLATE_DRINK_NAME = 'chocolate';
+
+    public const TEA_DRINK_PRICE = 0.4;
+    public const COFFEE_DRINK_PRICE = 0.5;
+    public const CHOCOLATE_DRINK_PRICE = 0.6;
+
+    public const DEFAULT_SUGAR = 0;
+    public const DEFAULT_EXTRA_HOT = false;
+
     protected function setUp(): void
     {
         parent::setUp();
@@ -16,63 +35,396 @@ class MakeDrinkCommandTest extends IntegrationTestCase
     }
 
     /**
-     * @dataProvider ordersProvider
-     * @param string $drinkType
-     * @param string $money
-     * @param int $sugars
-     * @param string $extraHot
-     * @param string $expectedOutput
+     * @dataProvider correctDrinksProvider
      */
-    public function testCoffeeMachineReturnsTheExpectedOutput(
+    public function testCoffeeMachineAcceptsCorrectDrink(
         string $drinkType,
-        string $money,
+        float $money,
         int $sugars,
-        string $extraHot,
-        string $expectedOutput
-    ): void {
-        $command = $this->application->find('app:order-drink');
-        $commandTester = new CommandTester($command);
-        $commandTester->execute(array(
-            'command'  => $command->getName(),
+        bool $extraHot,
+        string $expectedOutput,
+    ) {
+        $output = $this->runCommandMapArgs($drinkType, $money, $sugars, $extraHot);
 
-            // pass arguments to the helper
-            'drink-type' => $drinkType,
-            'money' => $money,
-            'sugars' => $sugars,
-            '--extra-hot' => $extraHot
-        ));
-
-        // the output of the command in the console
-        $output = $commandTester->getDisplay();
         $this->assertSame($expectedOutput, $output);
     }
 
-    public function ordersProvider(): array
+    /**
+     * @dataProvider incorrectDrinksProvider
+     */
+    public function testCoffeeMachineAcceptsIncorrectDrink(
+        string $drinkType,
+        float $money,
+        int $sugars,
+        bool $extraHot,
+        string $expectedOutput,
+    ) {
+        $output = $this->runCommandMapArgs($drinkType, $money, $sugars, $extraHot);
+
+        $this->assertSame($expectedOutput, $output);
+    }
+
+    /**
+     * @dataProvider correctMoneyProvider
+     */
+    public function testCoffeeMachineAcceptsCorrectMoney(
+        string $drinkType,
+        float $money,
+        int $sugars,
+        bool $extraHot,
+        string $expectedOutput,
+    ) {
+        $output = $this->runCommandMapArgs($drinkType, $money, $sugars, $extraHot);
+
+        $this->assertSame($expectedOutput, $output);
+    }
+
+    /**
+     * @dataProvider incorrectMoneyProvider
+     */
+    public function testCoffeeMachineAcceptsIncorrectMoney(
+        string $drinkType,
+        float $money,
+        int $sugars,
+        bool $extraHot,
+        string $expectedOutput,
+    ) {
+        $output = $this->runCommandMapArgs($drinkType, $money, $sugars, $extraHot);
+
+        $this->assertSame($expectedOutput, $output);
+    }
+
+    /**
+     * @dataProvider correctSugarProvider
+     */
+    public function testCoffeeMachineAcceptsCorrectSugar(
+        string $drinkType,
+        float $money,
+        int $sugars,
+        bool $extraHot,
+        string $expectedOutput,
+    ) {
+        $output = $this->runCommandMapArgs($drinkType, $money, $sugars, $extraHot);
+
+        $this->assertSame($expectedOutput, $output);
+    }
+
+    /**
+     * @dataProvider noSugarProvider
+     */
+    public function testCoffeeMachineAcceptsDefaultSugar(
+        string $drinkType,
+        float $money,
+        bool $extraHot,
+        string $expectedOutput,
+    ) {
+        $output = $this->runCommand([
+            'drink-type' => $drinkType,
+            'money' => $money,
+            '--extra-hot' => $extraHot,
+        ]);
+
+        $this->assertSame($expectedOutput, $output);
+    }
+
+    /**
+     * @dataProvider incorrectSugarProvider
+     */
+    public function testCoffeeMachineAcceptsIncorrectSugar(
+        string $drinkType,
+        float $money,
+        int $sugars,
+        bool $extraHot,
+        string $expectedOutput,
+    ) {
+        $output = $this->runCommandMapArgs($drinkType, $money, $sugars, $extraHot);
+
+        $this->assertSame($expectedOutput, $output);
+    }
+
+    /**
+     * @dataProvider withExtraHotProvider
+     */
+    public function testCoffeeMachineAcceptsExtraHot(
+        string $drinkType,
+        float $money,
+        int $sugars,
+        bool $extraHot,
+        string $expectedOutput,
+    ) {
+        $output = $this->runCommandMapArgs($drinkType, $money, $sugars, $extraHot);
+
+        $this->assertSame($expectedOutput, $output);
+    }
+
+    /**
+     * @dataProvider withoutExtraHotProvider
+     */
+    public function testCoffeeMachineAcceptsDefaultExtraHot(
+        string $drinkType,
+        float $money,
+        int $sugars,
+        string $expectedOutput,
+    ) {
+        $output = $this->runCommand([
+            'drink-type' => $drinkType,
+            'money' => $money,
+            'sugars' => $sugars,
+        ]);
+
+        $this->assertSame($expectedOutput, $output);
+    }
+
+    /**
+     * @dataProvider correctSugarAndExtraHotProvider
+     */
+    public function testCoffeeMachineAcceptsSugarAndExtraHot(
+        string $drinkType,
+        float $money,
+        int $sugars,
+        bool $extraHot,
+        string $expectedOutput,
+    ) {
+        $output = $this->runCommandMapArgs($drinkType, $money, $sugars, $extraHot);
+
+        $this->assertSame($expectedOutput, $output);
+    }
+
+    private function runCommand(array $args): string
+    {
+        $command = $this->application->find(static::COMMAND);
+        $commandTester = new CommandTester($command);
+
+        $commandTester->execute([
+            'command' => $command->getName(),
+
+            ...$args,
+        ]);
+
+        return $commandTester->getDisplay();
+    }
+
+    private function runCommandMapArgs(string $drinkType, float $money, int $sugars, bool $extraHot): string
+    {
+        return $this->runCommand([
+            'drink-type' => $drinkType,
+            'money' => $money,
+            'sugars' => $sugars,
+            '--extra-hot' => $extraHot,
+        ]);
+    }
+
+    private function correctDrinksProvider(): array
+    {
+        $outputPrefix = 'You have ordered a ';
+
+        return [
+            [
+                static::TEA_DRINK_NAME, static::TEA_DRINK_PRICE, static::DEFAULT_SUGAR, static::DEFAULT_EXTRA_HOT, $outputPrefix . static::TEA_DRINK_NAME . PHP_EOL,
+            ],
+            [
+                static::COFFEE_DRINK_NAME, static::COFFEE_DRINK_PRICE, static::DEFAULT_SUGAR, static::DEFAULT_EXTRA_HOT, $outputPrefix . static::COFFEE_DRINK_NAME . PHP_EOL,
+            ],
+            [
+                static::CHOCOLATE_DRINK_NAME, static::CHOCOLATE_DRINK_PRICE, static::DEFAULT_SUGAR, static::DEFAULT_EXTRA_HOT, $outputPrefix . static::CHOCOLATE_DRINK_NAME . PHP_EOL,
+            ],
+        ];
+    }
+
+    private function incorrectDrinksProvider(): array
+    {
+        $expectedOutput = 'The drink type should be ' .
+            static::TEA_DRINK_NAME . ', ' .
+            static::COFFEE_DRINK_NAME . ' or ' .
+            static::CHOCOLATE_DRINK_NAME . '.' . PHP_EOL;
+
+        return [
+            [
+                'team', static::TEA_DRINK_PRICE, static::DEFAULT_SUGAR, static::DEFAULT_EXTRA_HOT, $expectedOutput,
+            ],
+            [
+                '1', static::COFFEE_DRINK_PRICE, static::DEFAULT_SUGAR, static::DEFAULT_EXTRA_HOT, $expectedOutput,
+            ],
+            [
+                '', static::CHOCOLATE_DRINK_PRICE, static::DEFAULT_SUGAR, static::DEFAULT_EXTRA_HOT, $expectedOutput,
+            ],
+        ];
+    }
+
+    private function correctMoneyProvider(): array
+    {
+        $outputPrefix = 'You have ordered a ';
+
+        return [
+            [
+                static::TEA_DRINK_NAME, static::TEA_DRINK_PRICE, static::DEFAULT_SUGAR, static::DEFAULT_EXTRA_HOT, $outputPrefix . static::TEA_DRINK_NAME . PHP_EOL,
+            ],
+            [
+                static::TEA_DRINK_NAME, static::TEA_DRINK_PRICE + 1, static::DEFAULT_SUGAR, static::DEFAULT_EXTRA_HOT, $outputPrefix . static::TEA_DRINK_NAME . PHP_EOL,
+            ],
+
+            [
+                static::COFFEE_DRINK_NAME, static::COFFEE_DRINK_PRICE, static::DEFAULT_SUGAR, static::DEFAULT_EXTRA_HOT, $outputPrefix . static::COFFEE_DRINK_NAME . PHP_EOL,
+            ],
+            [
+                static::COFFEE_DRINK_NAME, static::COFFEE_DRINK_PRICE + 1, static::DEFAULT_SUGAR, static::DEFAULT_EXTRA_HOT, $outputPrefix . static::COFFEE_DRINK_NAME . PHP_EOL,
+            ],
+
+            [
+                static::CHOCOLATE_DRINK_NAME, static::CHOCOLATE_DRINK_PRICE, static::DEFAULT_SUGAR, static::DEFAULT_EXTRA_HOT, $outputPrefix . static::CHOCOLATE_DRINK_NAME . PHP_EOL,
+            ],
+            [
+                static::CHOCOLATE_DRINK_NAME, static::CHOCOLATE_DRINK_PRICE + 1, static::DEFAULT_SUGAR, static::DEFAULT_EXTRA_HOT, $outputPrefix . static::CHOCOLATE_DRINK_NAME . PHP_EOL,
+            ],
+        ];
+    }
+
+    private function incorrectMoneyProvider(): array
     {
         return [
             [
-                'chocolate', '0.7', 1, '', 'You have ordered a chocolate with 1 sugars (stick included)' . PHP_EOL
+                static::TEA_DRINK_NAME, 0.3, static::DEFAULT_SUGAR, static::DEFAULT_EXTRA_HOT, 'The ' . static::TEA_DRINK_NAME . ' costs ' . static::TEA_DRINK_PRICE . '.' . PHP_EOL,
             ],
             [
-                'tea', '0.4', 0, '1', 'You have ordered a tea extra hot' . PHP_EOL
+                static::TEA_DRINK_NAME, 0, static::DEFAULT_SUGAR, static::DEFAULT_EXTRA_HOT, 'The ' . static::TEA_DRINK_NAME . ' costs ' . static::TEA_DRINK_PRICE . '.' . PHP_EOL,
             ],
             [
-                'coffee', '2', 2, '1', 'You have ordered a coffee extra hot with 2 sugars (stick included)' . PHP_EOL
+                static::TEA_DRINK_NAME, -1, static::DEFAULT_SUGAR, static::DEFAULT_EXTRA_HOT, 'The ' . static::TEA_DRINK_NAME . ' costs ' . static::TEA_DRINK_PRICE . '.' . PHP_EOL,
+            ],
+
+            [
+                static::COFFEE_DRINK_NAME, 0.4, static::DEFAULT_SUGAR, static::DEFAULT_EXTRA_HOT, 'The ' . static::COFFEE_DRINK_NAME . ' costs ' . static::COFFEE_DRINK_PRICE . '.' . PHP_EOL,
             ],
             [
-                'coffee', '0.2', 2, '1', 'The coffee costs 0.5.' . PHP_EOL
+                static::COFFEE_DRINK_NAME, 0, static::DEFAULT_SUGAR, static::DEFAULT_EXTRA_HOT, 'The ' . static::COFFEE_DRINK_NAME . ' costs ' . static::COFFEE_DRINK_PRICE . '.' . PHP_EOL,
             ],
             [
-                'chocolate', '0.3', 2, '1', 'The chocolate costs 0.6.' . PHP_EOL
+                static::COFFEE_DRINK_NAME, -1, static::DEFAULT_SUGAR, static::DEFAULT_EXTRA_HOT, 'The ' . static::COFFEE_DRINK_NAME . ' costs ' . static::COFFEE_DRINK_PRICE . '.' . PHP_EOL,
+            ],
+
+            [
+                static::CHOCOLATE_DRINK_NAME, 0.5, static::DEFAULT_SUGAR, static::DEFAULT_EXTRA_HOT, 'The ' . static::CHOCOLATE_DRINK_NAME . ' costs ' . static::CHOCOLATE_DRINK_PRICE . '.' . PHP_EOL,
             ],
             [
-                'tea', '0.1', 2, '1', 'The tea costs 0.4.' . PHP_EOL
+                static::CHOCOLATE_DRINK_NAME, 0, static::DEFAULT_SUGAR, static::DEFAULT_EXTRA_HOT, 'The ' . static::CHOCOLATE_DRINK_NAME . ' costs ' . static::CHOCOLATE_DRINK_PRICE . '.' . PHP_EOL,
             ],
             [
-                'tea', '0.5', -1, '1', 'The number of sugars should be between 0 and 2.' . PHP_EOL
+                static::CHOCOLATE_DRINK_NAME, -1, static::DEFAULT_SUGAR, static::DEFAULT_EXTRA_HOT, 'The ' . static::CHOCOLATE_DRINK_NAME . ' costs ' . static::CHOCOLATE_DRINK_PRICE . '.' . PHP_EOL,
+            ],
+        ];
+    }
+
+    private function correctSugarProvider(): array
+    {
+        $noSugarOutputPrefix = 'You have ordered a ';
+        $outputPrefix = 'You have ordered a ';
+        $outputSuffix = 'sugars (stick included)';
+
+        return [
+            [
+                static::TEA_DRINK_NAME, static::TEA_DRINK_PRICE, 0, static::DEFAULT_EXTRA_HOT, $noSugarOutputPrefix . static::TEA_DRINK_NAME . PHP_EOL,
             ],
             [
-                'tea', '0.5', 3, '1', 'The number of sugars should be between 0 and 2.' . PHP_EOL
+                static::COFFEE_DRINK_NAME, static::COFFEE_DRINK_PRICE, 1, static::DEFAULT_EXTRA_HOT, $outputPrefix . static::COFFEE_DRINK_NAME . " with 1 {$outputSuffix}" . PHP_EOL,
+            ],
+            [
+                static::CHOCOLATE_DRINK_NAME, static::CHOCOLATE_DRINK_PRICE, 2, static::DEFAULT_EXTRA_HOT, $outputPrefix . static::CHOCOLATE_DRINK_NAME . " with 2 {$outputSuffix}" . PHP_EOL,
+            ],
+        ];
+    }
+
+    private function noSugarProvider(): array
+    {
+        $noSugarOutputPrefix = 'You have ordered a ';
+
+        return [
+            [
+                static::TEA_DRINK_NAME, static::TEA_DRINK_PRICE, static::DEFAULT_EXTRA_HOT, $noSugarOutputPrefix . static::TEA_DRINK_NAME . PHP_EOL,
+            ],
+            [
+                static::COFFEE_DRINK_NAME, static::COFFEE_DRINK_PRICE, static::DEFAULT_EXTRA_HOT, $noSugarOutputPrefix . static::COFFEE_DRINK_NAME . PHP_EOL,
+            ],
+            [
+                static::CHOCOLATE_DRINK_NAME, static::CHOCOLATE_DRINK_PRICE, static::DEFAULT_EXTRA_HOT, $noSugarOutputPrefix . static::CHOCOLATE_DRINK_NAME . PHP_EOL,
+            ],
+        ];
+    }
+
+    private function incorrectSugarProvider(): array
+    {
+        $expectedOutput = 'The number of sugars should be between 0 and 2.' . PHP_EOL;
+
+        return [
+            [
+                static::TEA_DRINK_NAME, static::TEA_DRINK_PRICE, 3, static::DEFAULT_EXTRA_HOT, $expectedOutput,
+            ],
+            [
+                static::COFFEE_DRINK_NAME, static::COFFEE_DRINK_PRICE, -1, static::DEFAULT_EXTRA_HOT, $expectedOutput,
+            ],
+        ];
+    }
+
+    private function withExtraHotProvider(): array
+    {
+        $outputPrefix = 'You have ordered a ';
+        $outputSuffix = ' extra hot';
+
+        return [
+            [
+                static::TEA_DRINK_NAME, static::TEA_DRINK_PRICE, static::DEFAULT_SUGAR, true, $outputPrefix . static::TEA_DRINK_NAME . $outputSuffix . PHP_EOL,
+            ],
+            [
+                static::COFFEE_DRINK_NAME, static::COFFEE_DRINK_PRICE, static::DEFAULT_SUGAR, true, $outputPrefix . static::COFFEE_DRINK_NAME . $outputSuffix . PHP_EOL,
+            ],
+            [
+                static::CHOCOLATE_DRINK_NAME, static::CHOCOLATE_DRINK_PRICE, static::DEFAULT_SUGAR, true, $outputPrefix . static::CHOCOLATE_DRINK_NAME . $outputSuffix . PHP_EOL,
+            ],
+        ];
+    }
+
+    private function withoutExtraHotProvider(): array
+    {
+        $outputPrefix = 'You have ordered a ';
+
+        return [
+            [
+                static::TEA_DRINK_NAME, static::TEA_DRINK_PRICE, static::DEFAULT_SUGAR, $outputPrefix . static::TEA_DRINK_NAME . PHP_EOL,
+            ],
+            [
+                static::COFFEE_DRINK_NAME, static::COFFEE_DRINK_PRICE, static::DEFAULT_SUGAR, $outputPrefix . static::COFFEE_DRINK_NAME . PHP_EOL,
+            ],
+            [
+                static::CHOCOLATE_DRINK_NAME, static::CHOCOLATE_DRINK_PRICE, static::DEFAULT_SUGAR, $outputPrefix . static::CHOCOLATE_DRINK_NAME . PHP_EOL,
+            ],
+        ];
+    }
+
+    private function correctSugarAndExtraHotProvider(): array
+    {
+        $outputPrefix = 'You have ordered a ';
+        $outputExtraHotSuffix = ' extra hot with ';
+        $outputSugarSuffix = ' sugars (stick included)' . PHP_EOL;
+
+        return [
+            [
+                static::TEA_DRINK_NAME, static::TEA_DRINK_PRICE, 1, true, $outputPrefix . static::TEA_DRINK_NAME . "{$outputExtraHotSuffix}1{$outputSugarSuffix}",
+            ],
+            [
+                static::TEA_DRINK_NAME, static::TEA_DRINK_PRICE, 2, true, $outputPrefix . static::TEA_DRINK_NAME . "{$outputExtraHotSuffix}2{$outputSugarSuffix}",
+            ],
+
+            [
+                static::COFFEE_DRINK_NAME, static::COFFEE_DRINK_PRICE, 1, true, $outputPrefix . static::COFFEE_DRINK_NAME . "{$outputExtraHotSuffix}1{$outputSugarSuffix}",
+            ],
+            [
+                static::COFFEE_DRINK_NAME, static::COFFEE_DRINK_PRICE, 2, true, $outputPrefix . static::COFFEE_DRINK_NAME . "{$outputExtraHotSuffix}2{$outputSugarSuffix}",
+            ],
+
+            [
+                static::CHOCOLATE_DRINK_NAME, static::CHOCOLATE_DRINK_PRICE, 1, true, $outputPrefix . static::CHOCOLATE_DRINK_NAME . "{$outputExtraHotSuffix}1{$outputSugarSuffix}",
+            ],
+            [
+                static::CHOCOLATE_DRINK_NAME, static::CHOCOLATE_DRINK_PRICE, 2, true, $outputPrefix . static::CHOCOLATE_DRINK_NAME . "{$outputExtraHotSuffix}2{$outputSugarSuffix}",
             ],
         ];
     }
--- tests/Integration/IntegrationTestCase.php
+++ tests/Integration/IntegrationTestCase.php
@@ -1,24 +1,24 @@
 <?php
 
+declare(strict_types=1);
+
 namespace GetWith\CoffeeMachine\Tests\Integration;
 
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Console\Application;
 
+/**
+ * @internal
+ * @coversNothing
+ */
 class IntegrationTestCase extends TestCase
 {
-    /** @var Application */
-    protected $application;
+    protected Application $application;
 
     protected function setUp(): void
     {
         parent::setUp();
 
         $this->application = new Application();
-    }
-
-    protected function tearDown(): void
-    {
-        parent::tearDown();
     }
 }
