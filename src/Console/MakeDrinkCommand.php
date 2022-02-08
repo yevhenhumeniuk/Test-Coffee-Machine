79f3ea0b29d55561d150fb34b9efc57fa0f85574
Yevhen Humeniuk <gumeniukyevgen@gmail.com>
2022-02-08 14:21:03 +0200

    updated make drink command

--- src/Console/MakeDrinkCommand.php
+++ src/Console/MakeDrinkCommand.php
@@ -1,7 +1,18 @@
 <?php
 
+declare(strict_types=1);
+
 namespace GetWith\CoffeeMachine\Console;
 
+use GetWith\CoffeeMachine\Drink\Contract\ResponseOrderMessagePresenterInterface;
+use GetWith\CoffeeMachine\Drink\Contract\ValidationMessagePresenterInterface;
+use GetWith\CoffeeMachine\Drink\Contract\ValidatorInterface;
+use GetWith\CoffeeMachine\Drink\Factory\ResponsePresenterFactory;
+use GetWith\CoffeeMachine\Drink\Factory\ValidationPresenterFactory;
+use GetWith\CoffeeMachine\Drink\Factory\ValidatorFactory;
+use GetWith\CoffeeMachine\Drink\MakeDrinkCommand as MakeDrinkCmd;
+use GetWith\CoffeeMachine\Drink\Presenter\ConsoleInputToDtoPresenter;
+use GetWith\CoffeeMachine\Drink\Presenter\DrinkInputToOrderDtoPresenter;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -10,95 +21,70 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class MakeDrinkCommand extends Command
 {
-    protected static $defaultName = 'app:order-drink';
+    protected static $defaultName = MakeDrinkCmd::NAME;
+
+    private ConsoleInputToDtoPresenter $inputPresenter;
+    private ValidatorInterface $validator;
+    private ValidationMessagePresenterInterface $validationPresenter;
+    private DrinkInputToOrderDtoPresenter $orderPresenter;
+    private ResponseOrderMessagePresenterInterface $outputPresenter;
 
     public function __construct()
     {
         parent::__construct(MakeDrinkCommand::$defaultName);
-    }
 
+        $this->inputPresenter = ConsoleInputToDtoPresenter::create();
+        $this->validator = ValidatorFactory::create()->makeDrinkValidator();
+        $this->validationPresenter = ValidationPresenterFactory::create()->makeConsoleMessagePresenter();
+        $this->orderPresenter = DrinkInputToOrderDtoPresenter::create();
+        $this->outputPresenter = ResponsePresenterFactory::create()->makeConsolePresenter();
+    }
 
     protected function configure(): void
     {
         $this->addArgument(
-            'drink-type',
+            MakeDrinkCmd::DRINK_TYPE_ARG,
             InputArgument::REQUIRED,
             'The type of the drink. (Tea, Coffee or Chocolate)'
         );
 
         $this->addArgument(
-            'money',
+            MakeDrinkCmd::MONEY_ARG,
             InputArgument::REQUIRED,
             'The amount of money given by the user'
         );
 
         $this->addArgument(
-            'sugars',
+            MakeDrinkCmd::SUGAR_ARG,
             InputArgument::OPTIONAL,
             'The number of sugars you want. (0, 1, 2)',
             0
         );
 
         $this->addOption(
-            'extra-hot',
-            'e',
+            MakeDrinkCmd::EXTRA_HOT_OPTION,
+            MakeDrinkCmd::EXTRA_HOT_OPTION_SHORT,
             InputOption::VALUE_NONE,
-            $description = 'If the user wants to make the drink extra hot'
+            'If the user wants to make the drink extra hot'
         );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $drinkType = strtolower($input->getArgument('drink-type'));
-        if (!in_array($drinkType, ['tea', 'coffee', 'chocolate'])) {
-            $output->writeln('The drink type should be tea, coffee or chocolate.');
-        } else {
-            /**
-             * Tea       --> 0.4
-             * Coffee    --> 0.5
-             * Chocolate --> 0.6
-             */
-            $money = $input->getArgument('money');
-            switch ($drinkType) {
-                case 'tea':
-                    if ($money < 0.4) {
-                        $output->writeln('The tea costs 0.4.');
-                        return 0;
-                    }
-                    break;
-                case 'coffee':
-                    if ($money < 0.5) {
-                        $output->writeln('The coffee costs 0.5.');
-                        return 0;
-                    }
-                    break;
-                case 'chocolate':
-                    if ($money < 0.6) {
-                        $output->writeln('The chocolate costs 0.6.');
-                        return 0;
-                    }
-                    break;
-            }
+        $inputDto = $this->inputPresenter->format($input);
+        $errorsBag = $this->validator->validate($inputDto);
+        if (!$errorsBag->isEmpty()) {
+            $message = $this->validationPresenter->format($errorsBag);
+            $output->writeln($message);
 
-            $sugars = $input->getArgument('sugars');
-            $stick = false;
-            $extraHot = $input->getOption('extra-hot');
-            if ($sugars >= 0 && $sugars <= 2) {
-                $output->write('You have ordered a ' . $drinkType);
-                if ($extraHot) {
-                    $output->write(' extra hot');
-                }
-
-                if ($sugars > 0) {
-                    $stick = true;
-                    $output->write(' with ' . $sugars . ' sugars (stick included)');
-                }
-                $output->writeln('');
-            } else {
-                $output->writeln('The number of sugars should be between 0 and 2.');
-            }
+            return self::SUCCESS;
         }
 
-        return 0;
+        $orderDto = $this->orderPresenter->format($inputDto);
+        // todo store the order (collect)
+        $message = $this->outputPresenter->format($orderDto);
+        $output->writeln($message);
+
+        return self::SUCCESS;
     }
 }
